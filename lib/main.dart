bd7861f4bb39116eb8028d4af7e1110aa560b309
j-eissler <57557568+j-eissler@users.noreply.github.com>
2022-04-08 23:41:09 +0200

    Add color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)),
       initialRoute: '/',
       routes: {
         '/': (context) => const OverviewScreen(),
